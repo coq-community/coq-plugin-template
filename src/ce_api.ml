2800fe724039570864b8bc46dac0db3aaf0461a8
Emilio Jesus Gallego Arias <e+git@x80.org>
2019-04-19 20:48:10 +0200

    [ocaml] Minor tweak in the code.

--- src/ce_api.ml
+++ src/ce_api.ml
@@ -1,11 +1,16 @@
+(** Example: calling a C function *)
 external get_n : unit -> int = "get_n"
 
 let callC () : int = get_n ()
 
+(** Example: simple, no-op tactic + print *)
 module PV = Proofview
+
+let msg_in_tactic str : unit PV.tactic =
+  PV.tclLIFT (PV.NonLogical.make (fun () ->
+      Feedback.msg_warning (Pp.str str)))
 
 let printHello : unit PV.tactic =
   let open PV.Notations in
-  PV.(tclLIFT (NonLogical.make (fun () ->
-      Feedback.msg_warning (Pp.str "hello")))) >>= fun () ->
+  msg_in_tactic "hello" >>= fun () ->
   Tacticals.New.tclIDTAC
